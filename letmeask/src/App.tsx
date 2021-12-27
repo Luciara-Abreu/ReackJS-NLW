0996143f72141a852a0338bbc69620578be33591
Luciara-Abreu <luciara.abreu@gmail.com>
2021-12-27 11:08:45 -0300

    Criação do componente Button de exemplo

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,7 +1,15 @@
+import { Button } from "./components/Button";
 
 function App() {
   return (
-    <h1>Ola Mundo mundo!!</h1>
+    <div>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </div>
 
   );
 }
