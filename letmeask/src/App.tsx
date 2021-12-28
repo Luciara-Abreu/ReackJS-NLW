a4711e5b3b4b44c0a333cdc71271df02f060ff9d
Luciara-Abreu <luciara.abreu@gmail.com>
2021-12-28 18:49:29 -0300

    Recuperação do estado de autenticação no firebase

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
@@ -21,6 +21,24 @@ export const AuthContext = createContext({} as AuthContextType);
 
 function App() {
   const [user, setUser] = useState<User>();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        const { displayName, photoURL, uid } = user;
+        if (!displayName || !photoURL) {
+          throw new Error('Missing information from Google Account.');
+        }
+        //Se caso der erro é porque faltou declarar as tipagens 
+        setUser({
+          id: uid,
+          name: displayName,
+          avatar: photoURL
+        })
+      }
+    })
+  }, [])
+
   //função de login
   async function singInWithGoogle() {
     const provider = new firebase.auth.GoogleAuthProvider();
